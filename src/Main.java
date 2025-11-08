987514c2286ec16fcca74ded53be7a39c99ccee8
Liriel Felix <67843062+Liriel-Felix@users.noreply.github.com>
2025-11-08 14:53:15 -0300

    feat: add test data

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import model.Cliente;
+import model.Conta;
+import model.ContaCorrente;
+import model.ContaPoupanca;
 import service.*;
 
 public class Main {
     public static void main(String[] args) {
         Banco banco = new BancoService();
-        // To do
+        
+        banco.cadastrarCliente("Ana Silva", "11111111111");
+        banco.cadastrarCliente("Bruno Costa", "22222222222");
+        System.out.println();
+
+        Conta conta1 = new ContaCorrente(1001, new Cliente("Ana Silva", "11111111111"), 500.0);
+        Conta conta2 = new ContaPoupanca(1002, new Cliente("Bruno Costa", "22222222222"), 300.0, 0.02);
+        banco.cadastrarConta(conta1);
+        banco.cadastrarConta(conta2);
+        System.out.println();
+
+        banco.depositar(1001, 200.0);
+        banco.sacar(1001, 100.0);
+        banco.transferir(1001, 1002, 150.0);
+        banco.aplicarRendimento();
+        System.out.println();
+
+        banco.consultarSaldo(1001);
+        banco.consultarSaldo(1002);
+        System.out.println();
+
+        banco.listarContas();
+        System.out.println();
+
+        banco.gerarRelatorio();
+        System.out.println();
     }
 }
